6d5709cf8c764d6b3260d241baefeb70494a900d
AnnaMykhailova <97619062+AnnaMykhailova@users.noreply.github.com>
2022-01-12 18:24:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const a = document.getElementById('numA');
+const b = document.getElementById('numB');
+const select = document.getElementById('selectProp');
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+
+
+btn.addEventListener('click', () => {
+    if (!a.value || !b.value) {
+        alert("Fill the Fields!")
+    } else {
+        if (select.value === "+") {
+            const sum = Number(a.value) + Number(b.value);
+            result.innerHTML = +sum.toFixed(2);
+        }
+
+        if (select.value === "-")  {
+            const sum = Number(a.value) - Number(b.value);
+            result.innerHTML = +sum.toFixed(2);
+        }
+
+        if (select.value === "*")  {
+            const sum = Number(a.value) * Number(b.value);
+            result.innerHTML = +sum.toFixed(2);
+        }
+
+        if (select.value === "/")  {
+            const sum = Number(a.value) / Number(b.value);
+            result.innerHTML = +sum.toFixed(2);
+        }
+
+    }
+})
